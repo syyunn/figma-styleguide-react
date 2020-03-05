9fb995f6f74dd1a924c285a99811457985f20395
zachary <syyun@snu.ac.kr>
2020-03-05 11:39:10 +0900

    Add shapes

---
+++ src/stories/Shapes.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default {
+    title: 'Colors',
+    parameters: {
+        backgrounds: [
+            { name: 'default', value: '#e6e6e6', default: true },
+        ]
+    },
+};
