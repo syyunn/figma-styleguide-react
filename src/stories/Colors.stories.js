6275151c207cbc4a3484ad5bb0bceeae6a304351
zachary <syyun@snu.ac.kr>
2020-03-06 21:34:45 +0900

    Change with Rectangle

--- src/stories/Colors.stories.js
+++ src/stories/Colors.stories.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Rectangle, RectangleTextCenter } from './Shapes.stories'
+import { Text } from './Typography.stories'
 
 export default {
     title: 'Colors',
@@ -9,24 +11,13 @@ export default {
     },
 };
 
-export const Color = ({ bgColor = '#000000' }) => {
+export const Palette = ({ bgColor = '#000000' }) => {
     return (
         <>
-            <div style={{ background: bgColor, width: '160px', height: '140px', borderTopLeftRadius: '12px', borderTopRightRadius: '12px' }} />
-            <div style={{
-                width: '160px', height: '50px', borderBottomLeftRadius: '12px', borderBottomRightRadius: '12px', display: "flex", alignItems: 'center', justifyContent: 'center', background: '#FFFFFF'
-            }}>
-                < p style={{
-                    margin: 0,
-                    fontSize: "17px",
-                    fontFamily: "SF Pro Text",
-                    lineHeight: "22px",
-                }}>
-                    {bgColor}
-                </p>
-            </div >
+            <Rectangle bblRad='0px' bbrRad='0px' bgColor={bgColor} />
+            <RectangleTextCenter text={bgColor} height='50px' btlRad='0px' btrRad='0px' />
         </>
     )
 }
 
-export const Black = () => Color({ bgColor: '#BE52F2' })
+export const Black = () => Palette({ bgColor: '#BE52F2' })
